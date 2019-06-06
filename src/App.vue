7783ed34a8e814e9360f961006dfcf7a26c472c1
Marcin Filipowski <proartz.devon@gmail.com>
2019-06-06 12:10:28 +0200

    Component CSS

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>{{ greeting() }}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from './Ninjas.vue'
+
 export default {
+  components: {
+    'ninjas': Ninjas
+  },
   data () {
     return {
-      title: 'Your first Vue file, wooo!'
-    }
-  },
-  methods: {
-    greeting: function(){
-      return 'heeeey cowboy!';
+      title: 'Ninja App'
     }
   }
 }
-</script>
+</script scoped>
+<style>
+  h1{
+    color: purple;
+  }
+</style>
