b61e1fd3713c77aca91eb10be765cc0358661def
Marcin Filipowski <proartz.devon@gmail.com>
2019-06-07 12:42:11 +0200

    Hooking up to firebase(part 1)

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -56,11 +56,7 @@ export default {
   },
   methods: {
     post: function() {
-      this.$http.post('https://jsonplaceholder.typicode.com/posts', {
-        title: this.blog.title,
-        body: this.blog.content,
-        userId: 1
-      }).then(function(data){
+      this.$http.post('https://nn-vue-playlist-ae4aa.firebaseio.com/posts.json', this.blog).then(function(data){
         console.log(data);
         this.submitted = true;
       });
